524f3ae6ee13f9856f42c5175f7429edf25b69b6
KlineZachary <klinee101@gmail.com>
2020-01-15 15:15:48 -1000

    Added method

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        greeting("Hello world");
+    }
+
+    public static void greeting(String message){
+        System.out.println(message);
     }
 }
